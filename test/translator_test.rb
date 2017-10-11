43976f6cbc2698440ed4e86c97091aaf93932c8a
nico24687 <theonlynico@hotmail.com>
2017-10-11 10:04:54 -0600

    Iteration 1 test passing but needs refactor

---
+++ test/translator_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require '../lib/translator'
+
+
+class TranslatorTest < Minitest::Test 
+    def test_translator_class_exists
+        translator = Translator.new
+
+        assert_instance_of Translator,translator
+    end
+
+    def test_eng_to_morse_returns_morse_code
+        translator = Translator.new
+
+        assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world")
+    end
+
+    def test_eng_to_morse_is_case_insensitive
+        translator = Translator.new
+
+        assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("Hello World")
+    end
+
+    def test_eng_to_morse_works_with_numbers
+        translator = Translator.new
+
+        assert_equal "-......-.. .-.-.. ...-- ..........--....", translator.eng_to_morse("There are 3 ships")
+    end
+end
